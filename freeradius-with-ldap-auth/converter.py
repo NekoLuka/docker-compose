fab92bf5dbc56b7b229785c2150e73fb0a3160cf
Luka de Jong <52010222+NekoLuka@users.noreply.github.com>
2023-04-25 18:32:27 +0200

    Upgraded to swarm config

--- freeradius-with-ldap-auth/converter.py
+++ freeradius-with-ldap-auth/converter.py
@@ -1,9 +1,9 @@
 import json
 
-with open("/config/clients.json", "r") as clients:
+with open("/radius-clients.json", "r") as clients:
     clients = json.load(clients)
 
-with open("/config/clients.conf", "w") as conf:
+with open("/clients.conf", "w") as conf:
     for i in clients:
         conf.write(
             f"client {i['name']}" " {\n" f"\tipaddr = {i['ip']}\n"
---
+++ freeradius-with-ldap-auth/readme.md
@@ -0,0 +1,8 @@
+# Freeradius docker container with LDAP auth
+
+## Env vars
+`LDAP_HOST`: LDAP server hostname  
+`LDAP_PORT`: LDAP server port  
+`LDAP_USER`: LDAP server user  
+`LDAP_PASS_FILE`: LDAP server password file location  
+`LDAP_BASEDN`: LDAP server base DN
