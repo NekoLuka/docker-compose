e0f227c3b98c5f6b94187103a44b2356b03a48da
Luka de Jong <lukadejong2002@gmail.com>
2024-10-13 18:30:08 +0200

    Updated scripts to auto generate frps auth token

--- vps-setup/init.sh
+++ vps-setup/init.sh
@@ -1,6 +1,7 @@
 set -u
 
-export FRPS_AUTH_TOKEN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+frps_auth_token=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n)
+echo FRPS_AUTH_TOKEN=$frps_auth_token 1 > frps.env
 
 cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1 > authelia/secrets/JWT_SECRET
 cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1 > authelia/secrets/SESSION_SECRET
@@ -15,5 +16,6 @@ echo $ldap_authelia_password > authelia/secrets/LDAP_PASSWORD
 
 openssl genpkey -algorithm RSA -out lldap_config/private.key
 openssl req -x509 -days 3650 -key lldap_config/private.key -out lldap_config/certificate.crt
+echo Created key pair for LDAPS
 
 echo Create user "authelia_bind_user" in LLDAP with password: $ldap_authelia_password
